258f489d55616b55eecc0b0f84490023c778f1fe
Lonewolf <107942064+lonewolf-loopring@users.noreply.github.com>
2026-01-03 17:57:49 -0500

    Finalized contract with constuctor defaults

--- IceCreamShoppe.sol
+++ IceCreamShoppe.sol
@@ -7,12 +7,20 @@ import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/
 import {ERC1155Pausable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
 import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC2981} from "@openzeppelin/contracts/token/common/ERC2981.sol";
 
-contract IceCreamShoppe is ERC1155, Ownable, ERC1155Pausable, ERC1155Burnable, ERC1155Supply {
+contract IceCreamShoppe is ERC1155, Ownable, ERC1155Pausable, ERC1155Burnable, ERC1155Supply, ERC2981 {
+
+    string public name = "Ice Cream Shoppe";
+    string public symbol = "ICS";
+
     constructor(address initialOwner)
         ERC1155("https://myart.com/api/item/{id}.json")
         Ownable(initialOwner)
-    {}
+
+    {
+        _setDefaultRoyalty(initialOwner, 500);
+    }
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
@@ -40,7 +48,24 @@ contract IceCreamShoppe is ERC1155, Ownable, ERC1155Pausable, ERC1155Burnable, E
         _mintBatch(to, ids, amounts, data);
     }
 
+    // This allows you to change the royalty percentage or receiver address later
+    function setDefaultRoyalty(address receiver, uint96 feeNumerator)
+        public
+        onlyOwner
+    {
+        _setDefaultRoyalty(receiver, feeNumerator);
+    }
+
     // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC1155, ERC2981)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
     function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
         internal
