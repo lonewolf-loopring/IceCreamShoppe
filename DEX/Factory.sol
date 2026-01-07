2ab096988b6387f0564ce8c84b9f36a521ea1cd4
Lonewolf <107942064+lonewolf-loopring@users.noreply.github.com>
2026-01-07 05:33:48 -0500

    Initialized DexPair.sol & added Factory.sol logic

--- DEX/Factory.sol
+++ DEX/Factory.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./DexPair.sol";
 
 /**
- * @title ArtDexFactory
+ * @title DexFactory
  * @dev The factory contract for the Art DEX. 
  * Responsible for creating pairs and managing fee settings.
  */
-contract ArtDexFactory is Ownable {
+contract DexFactory is Ownable {
     address public feeTo;
     address public feeToSetter;
 
@@ -26,34 +27,41 @@ contract ArtDexFactory is Ownable {
         return allPairs.length;
     }
 
-    function setFeeTo(address _feeTo) external {
-        require(msg.sender == feeToSetter, "ArtDex: FORBIDDEN");
-        feeTo = _feeTo;
-    }
+/* Creates liquidity pair for two tokens  */
 
-    function setFeeToSetter(address _feeToSetter) external {
-        require(msg.sender == feeToSetter, "ArtDex: FORBIDDEN");
-        feeToSetter = _feeToSetter;
-    }
-
-    /**
-     * @dev Creates a liquidity pair for two tokens.
-     * In the future, this is where you will link $ART2, $BRUSH, etc.
-     */
     function createPair(address tokenA, address tokenB) external returns (address pair) {
-        require(tokenA != tokenB, "ArtDex: IDENTICAL_ADDRESSES");
+        require(tokenA != tokenB, "Dex: IDENTICAL_ADDRESSES");
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), "ArtDex: ZERO_ADDRESS");
-        require(getPair[token0][token1] == address(0), "ArtDex: PAIR_EXISTS");
+        require(token0 != address(0), "Dex: ZERO_ADDRESS");
+        require(getPair[token0][token1] == address(0), "Dex: PAIR_EXISTS");
 
         // Deployment logic for the Pair contract goes here (usually via create2)
         // For brevity in this initial component, we focus on the Factory structure.
         
         // ... [Pair Deployment Code] ...
 
+        bytes memory bytecode = type(DexPair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+
+        // Initialize the pair with the two tokens
+        IDexPair(pair).initialize(token0, token1);
+
         getPair[token0][token1] = pair;
         getPair[token1][token0] = pair; // populate mapping in both directions
         allPairs.push(pair);
         emit PairCreated(token0, token1, pair, allPairs.length);
     }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, "Dex: FORBIDDEN");
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, "Dex: FORBIDDEN");
+        feeToSetter = _feeToSetter;
+    }   
 }
